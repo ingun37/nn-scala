e1a88c61765afb483e1ef17115c4c4d325f298cc
Ingun <ingun37@gmail.com>
2020-08-02 22:01:10 +0900

    scalafmt

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,21 +3,20 @@ import cats.Apply
 import cats.implicits._
 import com.kubukoz.polskibus.domain.Matrix
 import scala.collection.immutable.Nil
-import scala.math.{exp,pow}
+import scala.math.{exp, pow}
 
 object Main extends App {
   trait Differentiable {
     val func: List[Double] => List[Double]
     val derivative: List[Double] => Matrix
 
-    def compose(g:Differentiable) : Differentiable = {
+    def compose(g: Differentiable): Differentiable = {
       val parent = this
       return new Differentiable {
         val func: List[Double] => List[Double] = parent.func compose g.func
-        val derivative: List[Double] => Matrix = a => parent.derivative(g.func(a)) * g.derivative(a)
-      } 
+        val derivative: List[Double] => Matrix = a =>
+          parent.derivative(g.func(a)) * g.derivative(a)
+      }
     }
   }
-
-  
 }
--- src/test/scala/Test.scala
+++ src/test/scala/Test.scala
@@ -3,26 +3,33 @@ import cats.Apply
 import cats.implicits._
 import com.kubukoz.polskibus.domain.Matrix
 import scala.collection.immutable.Nil
-import scala.math.{exp,pow}
+import scala.math.{exp, pow}
 import Main.Differentiable
 
 class CubeCalculatorTest extends org.scalatest.FunSuite {
-  test("CubeCalculator.cube") {
-      val f = new Differentiable{
-        val func: List[Double] => List[Double] = x => x match {
-        case x :: y :: _ => List(x * exp(x*y))
+  test("aoeu") {
+    val f = new Differentiable {
+      val func: List[Double] => List[Double] = x =>
+        x match {
+          case x :: y :: _ => List(x * exp(x * y))
         }
-        val derivative: List[Double] => Matrix = x => x match {
-        case x :: y :: next => Matrix( List(List(exp(x*y) + y*x*exp(x*y), x*x*exp(x*y))))
+      val derivative: List[Double] => Matrix = x =>
+        x match {
+          case x :: y :: next =>
+            Matrix(
+              List(List(exp(x * y) + y * x * exp(x * y), x * x * exp(x * y)))
+            )
         }
     }
 
     val g = new Differentiable {
-        val func: List[Double] => List[Double] = x => x match {
-        case t :: next => List(t*t, 1/t)
+      val func: List[Double] => List[Double] = x =>
+        x match {
+          case t :: next => List(t * t, 1 / t)
         }
-        val derivative: List[Double] => Matrix = x => x match {
-        case t :: next => Matrix(List(List(2*t),List(-pow(t,-2))))
+      val derivative: List[Double] => Matrix = x =>
+        x match {
+          case t :: next => Matrix(List(List(2 * t), List(-pow(t, -2))))
         }
     }
 
@@ -30,4 +37,5 @@ class CubeCalculatorTest extends org.scalatest.FunSuite {
     assert(fg.func(List(4)) == List(873.5704005303078))
     assert(fg.derivative(List(4)) == Matrix(List(List(1310.3556007954617))))
   }
+  test("htns") {}
 }
